71198fb762f3fda6b775c65f14d2f4c864bfd224
bhoomisharmaa <bhoomisharma404@gmail.com>
2024-06-22 02:34:50 +0530

    heeeeeeeeeeeeee

--- src/filter-box.js
+++ src/filter-box.js
@@ -9,19 +9,19 @@ export default function FilterBox({ mousePosX, mousePosY, setShowFilterBox }) {
       onClick={() => setShowFilterBox(false)}
     >
       <div
-        className="right-click-div gap-3 text-sm py-2 px-2.5"
+        className="right-click-div gap-1 text-sm py-2 px-2"
         style={{ left: mousePosX - 20, top: mousePosY + 10 }}
       >
-        <button className="flex gap-2 items-center">
+        <button className="buttons flex gap-2 items-center">
           <BacklogSvg /> <span>Status</span>
         </button>
-        <button className="flex gap-2 items-center">
+        <button className="buttons flex gap-2 items-center">
           <HighPrioritySvg /> <span>Priority</span>
         </button>
-        <button className="flex gap-4 items-center">
+        <button className="buttons flex items-center" style={{ gap: "16px" }}>
           <AddLabelSvg /> <span>Label</span>
         </button>
-        <button className="flex gap-2 items-center">
+        <button className="buttons flex gap-2 items-center">
           <TextIcon /> <span>Content</span>
         </button>
       </div>
